8f577db83932c23248b7e3c17eeac7cccb8bb46b
User <User@WIN-OMKK526QHAQ>
2023-10-30 17:40:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = null;
+int old;
+string live;
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("What is your name?");
+name = Convert.ToString(Console.ReadLine());
+Console.WriteLine("My name is " + name);
+Console.WriteLine("Cool! And my name is Visual Studio");
+Console.WriteLine("How old are you?");
+old = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("I'm " + old + "years old");
+Console.WriteLine("I see. Where do you live?");
+live = Convert.ToString(Console.ReadLine());
+Console.WriteLine("I live in " + live);
+Console.WriteLine("Cool! I live there too");
+Console.WriteLine("Goodbye " + name);
+Console.WriteLine("Goodbye Visual Studio");
+
